6898159c643571493bc8d8760e470f8a492ff590
07Nurcahyo <bagusnurcahyo4@gmail.com>
2024-12-25 00:15:23 +0700

    update elbow curve menggunakan data yang telah di pra proses

--- elbow_curve.py
+++ elbow_curve.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # Load the uploaded CSV file to analyze its contents
 file_path = 'data kesejahteraan pekerja - normalized.csv'
+# file_path = 'elbow curve - hasil pra proses data.csv'
 data = pd.read_csv(file_path)
 # Display the first few rows of the dataset to understand its structure
 print(data.head(), data.shape)
@@ -15,22 +16,24 @@ print("\n",data.head())
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-# Range of k values to test
+# Jarak k untuk di tes
 k_values = range(1, 10)
 
-# Calculate inertia for each k
+# Menghitung inertia untuk setiap k
 inertia = []
 for k in k_values:
     kmeans = KMeans(n_clusters=k, random_state=42)
     kmeans.fit(data)
     inertia.append(kmeans.inertia_)
 
-# Plot the elbow curve
+# Plot elbow curve
 # plt.figure(figsize=(8, 5))
 plt.plot(k_values, inertia, marker='o', linestyle='--')
-plt.title('Elbow Curve for Optimal k')
-plt.xlabel('Number of Clusters (k)')
+plt.title('Elbow Curve untuk K Optimal')
+plt.xlabel('Clusters (k)')
 plt.ylabel('Inertia')
 plt.xticks(k_values)
 plt.grid()
 plt.show()
+
+
