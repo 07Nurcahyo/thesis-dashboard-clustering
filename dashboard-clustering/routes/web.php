5303b1b58e6f60c283027b57835a17226902f890
07Nurcahyo <bagusnurcahyo4@gmail.com>
2025-03-23 23:23:41 +0700

    update setup layouting : - make guest page - make admin page

--- dashboard-clustering/routes/web.php
+++ dashboard-clustering/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin_controller;
+use App\Http\Controllers\main_controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [main_controller::class, 'index']);
+Route::get('/admin', [admin_controller::class, 'index']);
