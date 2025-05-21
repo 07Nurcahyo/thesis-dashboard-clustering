c5ee8e5ef952bfdde5940f670bcf79343ede0e40
07Nurcahyo <bagusnurcahyo4@gmail.com>
2025-05-21 16:12:53 +0700

    update navbar admin

--- dashboard-clustering/resources/views/layouts/header.blade.php
+++ dashboard-clustering/resources/views/layouts/header.blade.php
@@ -5,17 +5,21 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="../../index3.html" class="nav-link">Home</a>
+        <a href="#" class="nav-link" onclick="window.scrollTo({top: 0, behavior: 'smooth'}); return false;">
+          <i class="fas fa-arrow-up"></i> Kembali Ke Atas
+        </a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i> Layar Penuh
+        </a>
       </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a class="nav-link" data-widget="navbar-search" href="#" role="button">
           <i class="fas fa-search"></i>
         </a>
@@ -34,10 +38,10 @@
             </div>
           </form>
         </div>
-      </li>
+      </li> --}}
 
       <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
+      {{-- <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
           <i class="far fa-bell"></i>
           <span class="badge badge-warning navbar-badge">15</span>
@@ -62,13 +66,8 @@
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item pl-5">
+      </li> --}}
+      <li class="nav-item pr-3">
         <a href="{{route('logout')}}" class="btn btn-outline-danger" style="border-radius: 10px"><i class="fas fa-sign-out-alt"></i>&nbsp;&nbsp;Logout</a>
       </li>
     </ul>
